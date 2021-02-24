499ea4749d537306a28e22e3819dcb1f9536df18
oherbert <o_herbert@hotmail.com>
2021-02-24 00:04:01 -0300

    Aceitando requiscoes json

--- API/src/server.ts
+++ API/src/server.ts
@@ -1,10 +1,11 @@
 import express from 'express';
+import "reflect-metadata";
+import "./database"
+import { router } from './routes';
 
 const app = express();
 
-app.get("/users", (request, response)=>
-{
-    return response.send("<h1>OK</h1>");
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, ()=> console.log("Server is running!"));
